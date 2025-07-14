2575f6bbce576e8822ec9ce8974a1a2261fc1bb7
nihar756 <niharranjanchetia022@gmail.com>
2025-07-14 14:33:08 +0000

    convert to binary  and maximum matching

---
+++ daily-challenge/max-matching.cpp
@@ -0,0 +1,28 @@
+// You are given a 0-indexed integer array players, where players[i] represents the ability of the ith player. You are also given a 0-indexed integer array trainers, where trainers[j] represents the training capacity of the jth trainer.
+
+// The ith player can match with the jth trainer if the player's ability is less than or equal to the trainer's training capacity. Additionally, the ith player can be matched with at most one trainer, and the jth trainer can be matched with at most one player.
+
+// Return the maximum number of matchings between players and trainers that satisfy these conditions
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        sort(players.begin(),players.end());
+        sort(trainers.begin(),trainers.end());
+
+        int i=0,j=0;
+        int n=players.size(),m=trainers.size();
+        while(i<n && j<m){
+            if(players[i]<=trainers[j]){
+                i++;j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return i;
+    }
+};
